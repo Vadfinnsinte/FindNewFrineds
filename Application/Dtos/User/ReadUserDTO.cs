f1311c49c2a7a9b5d0fc79c72df91a86cb5a6c7f
Linda Westerlund <lindawesterlund94@gmail.com>
2026-05-06 09:10:09 +0200

    added DTOs and added them to Mapper

---
+++ Application/Dtos/User/ReadUserDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Application.Dtos.User
+{
+    public class ReadUserDTO
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public int Age { get; set; }
+
+        public string Bio { get; set; }
+
+        public string City { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public string Fullname { get; set; }
+
+        public bool Lonely { get; set; }
+
+        public string Interests { get; set; }
+
+    }
+}
---
+++ Application/Mappings/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using Application.Dtos.Event;
+using Application.Dtos.Message;
+using Application.Dtos.User;
+using AutoMapper;
+using Domain.Models.Events;
+using Domain.Models.Messages;
+using Domain.Models.Users;
+using Microsoft.Extensions.Logging;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+
+        CreateMap<User, ReadUserDTO>();
+
+        CreateMap<UpdateUserDTO, User>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+  
+        CreateMap<AddEventDTO, Event>();
+
+
+        CreateMap<CreateMessageDTO, Message>()
+            .ForMember(dest => dest.SenderId, opt => opt.Ignore())
+            .ForMember(dest => dest.SentAt, opt => opt.Ignore())
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+    }
+}
