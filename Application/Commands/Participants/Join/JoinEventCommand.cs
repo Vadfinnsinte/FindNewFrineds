0b8f98cdd888009e6ed295bb38a24cf15b9fc13b
Linda Westerlund <lindawesterlund94@gmail.com>
2026-05-10 12:39:22 +0200

    Added connection between event and participant. Can join and leave. Also added det MY events.

---
+++ Application/Commands/Participants/Join/JoinEventCommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common;
+using MediatR;
+
+namespace Application.Commands.Participants.Join
+{
+    public class JoinEventCommand
+        : IRequest<OperationResult<string>>
+    {
+        public Guid EventId { get; set; }
+
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Infrasturcture/Migrations/20260510102018_changedentitys.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrasturcture.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedentitys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "Participants");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEI4hjOuDOPeVr7FfOvbhiJ2RDi3gYzRbtELCqYLi+sH56T0ePP1HPGmYfjjq+LCc3w==");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEI4hjOuDOPeVr7FfOvbhiJ2RDi3gYzRbtELCqYLi+sH56T0ePP1HPGmYfjjq+LCc3w==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "id",
+                table: "Participants",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "PasswordHash",
+                value: "SEEDED_HASH");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "PasswordHash",
+                value: "SEEDED_HASH");
+        }
+    }
+}
